d4f8b7f10806fd4ea0f1c252e7d151c007762ec0
Calin <Calin.Radu@endava.com>
2015-07-23 17:39:19 +0300

    Added Maven and Git training project

---
+++ MavenGit/src/main/java/com/java/simpleapp/App.java
@@ -0,0 +1,14 @@
+package com.java.simpleapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( "Random mesersage" );
+    }
+}
---
+++ MavenGit/web-app/src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Random message to prove me that I made it!</h2>
+</body>
+</html>
